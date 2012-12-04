f3cf95a0ed1c7c3fedd9e1c28927037b6011a54e
erikryverling <erik.yverling@smartbear.com>
2012-12-04 09:46:47 +0100

    Optimizing the CountryToCurrency script

--- groovy-webinar-2012-12-05/custom-groovy-library/scripts/com/smartbear/soapui/demo/CountryToCurrency.groovy
+++ groovy-webinar-2012-12-05/custom-groovy-library/scripts/com/smartbear/soapui/demo/CountryToCurrency.groovy
@@ -5,7 +5,7 @@ class CountryToCurrency {
         def map = ['Sweden':'SEK', 'Norway':'NOK']
         
         if(!map.containsKey(country))
-            throw new IllegalArgumentException("""Couldn't find currency for the provided country: ${country}""")
+            throw new IllegalArgumentException("Couldn't find currency for the provided country: $country")
         else
             return map[country]
     }
