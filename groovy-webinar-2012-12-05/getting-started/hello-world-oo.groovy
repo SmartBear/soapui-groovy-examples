b0d98336bb549594ee23df0465342faf7ab1e9d4
erikryverling <erik.yverling@smartbear.com>
2012-12-04 09:41:55 +0100

    Making the hello world OO example more readable

--- groovy-webinar-2012-12-05/getting-started/hello-world-oo.groovy
+++ groovy-webinar-2012-12-05/getting-started/hello-world-oo.groovy
@@ -1,8 +1,14 @@
 class Greet {
-  def name
-  Greet(who) { name = who[0].toUpperCase() + who[1..-1] }
-  def salute() { println "Hello $name!" }
+    def name
+    
+    Greet(who) { 
+        name = who[0].toUpperCase() + who[1..-1] 
+    }
+  
+    def salute() { 
+        println "Hello $name!" 
+    }
 }
 
-g = new Greet('world')  // create object
-g.salute()               // output "Hello World!"
+g = new Greet('world')      // create object
+g.salute()                  // output "Hello World!"
