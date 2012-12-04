b0d992893f9c4c09ceea464bc318afda64c06ab1
erikryverling <erik.yverling@smartbear.com>
2012-12-04 08:26:35 +0100

    * Adding Custom Groovy library example * Renaming camel cased Get and set properties project to a more reabable format

---
+++ groovy-webinar-2012-12-05/custom-groovy-library/scripts/com/smartbear/soapui/demo/CountryToCurrency.groovy
@@ -0,0 +1,12 @@
+package com.smartbear.soapui.demo;
+
+class CountryToCurrency {
+    def static String convert(String country) {        
+        def map = ['Sweden':'SEK', 'Norway':'NOK']
+        
+        if(!map.containsKey(country))
+            throw new IllegalArgumentException("""Couldn't find currency for the provided country: ${country}""")
+        else
+            return map[country]
+    }
+}
